392b328f28e22fe01a7fbef07fe638e4d258f42b
Varain-Gandikota <varain@outlook.com>
2025-05-25 23:53:30 -0500

    Did more gun stuff, big bug that needs fixing
    Renamed sample scene to MainTestScene

---
+++ Iron Oxide/Assets/Scripts/GunSpriteChanger.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GunSpriteChagner : MonoBehaviour
+{
+    private Animator gunAnimator;
+    private void Start()
+    {
+        gunAnimator = transform.GetChild(0).GetComponent<Animator>();
+    }
+    void FixedUpdate()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePosition - (Vector2)transform.position);
+        gunAnimator.SetFloat("X", direction.x);
+        gunAnimator.SetFloat("Y", direction.y);
+    }
+}
