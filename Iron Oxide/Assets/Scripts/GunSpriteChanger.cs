4cc1d9b8cb32884722d8f212384b6fe3814b83df
Varain-Gandikota <varain@tamu.edu>
2026-03-05 19:02:54 -0600

    Stuff

--- Iron Oxide/Assets/Scripts/GunSpriteChanger.cs
+++ Iron Oxide/Assets/Scripts/GunSpriteChanger.cs
@@ -2,15 +2,10 @@ using UnityEngine;
 
 public class GunSpriteChanger : MonoBehaviour
 {
-    private Animator gunAnimator;
-    private void Start()
-    {
-        gunAnimator = transform.GetChild(0).GetComponent<Animator>();
-    }
+    [SerializeField] private Animator gunAnimator;
     void FixedUpdate()
     {
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = (mousePosition - (Vector2)transform.position);
+        Vector2 direction = transform.right;
         gunAnimator.SetFloat("X", direction.x);
         gunAnimator.SetFloat("Y", direction.y);
     }
